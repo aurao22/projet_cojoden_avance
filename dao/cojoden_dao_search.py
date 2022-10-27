9836c0fefe0863ff297e34bbc103a7d5d551b267
Aurelie RAOUL <aurelie.raoul@yahoo.fr>
2022-10-27 16:04:37 +0200

    search_multicriteria

--- dao/cojoden_dao_search.py
+++ dao/cojoden_dao_search.py
@@ -1,13 +1,32 @@
 
+# %% doc
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" Module to search specifics datas
+
+Project: Cojoden avance
+=======
+
+Usage:
+======
+    python cojoden_dao_search.py
+"""
+__authors__     = ("Aurélie RAOUL")
+__contact__     = ("aurelie.raoul@yahoo.fr")
+__copyright__   = "MIT"
+__date__        = "2022-10-01"
+__version__     = "1.0.0"
+
 # %% import
 import pandas as pd
 import sys
+
 sys.path.append(r"C:\Users\User\WORK\workspace-ia\PROJETS\projet_cojoden_avance")
 from dao.cojoden_dao import executer_sql
 from tqdm import tqdm
 
 # ----------------------------------------------------------------------------------
-# %% RECHERCHE EN BDD
+# %% CONSTANTS
 # ----------------------------------------------------------------------------------
 
 LEVEL_COLS = {  
@@ -40,10 +59,49 @@ LEVEL_COLS = {
                         },
     }
 
+SEARCH_RESULT_COLS_BY_TABLE = {  
+        'ville'     :'ville.id as ville_id, ville.ville, ville.departement, ville.region1',
+        'oeuvre'    :'oeuvre.ref, oeuvre.titre, oeuvre.type, oeuvre.annee_debut, oeuvre.annee_fin, oeuvre.lieux_conservation, oeuvre.inscriptions, oeuvre.texte',
+        'metier'    :'metier.metier',
+        'materiaux_technique':'materiaux_technique.materiaux_technique',
+        'domaine'   :'domaine.domaine',
+        'artiste'   :'artiste.nom_naissance, artiste.dit',
+        'musee'     :"musee.museo, musee.nom, musee.ville as ville_musee, musee.latitude, musee.longitude"
+    }
 
-def search_multicriteria(ville=None, oeuvre=None, musee=None, metier=None, materiaux=None, domaine=None, artiste=None, type_oeuvre=None, search_strategie='AND', search_level=1, verbose=0):
+
+SEARCH_RESULT_COLS2 = {  
+        'ville'     :'ville.id as ville_id, ville.ville, ville.departement, ville.region1',
+        'oeuvre'    :'oeuvre.ref, oeuvre.titre, oeuvre.type, oeuvre.annee_debut, oeuvre.annee_fin, oeuvre.lieux_conservation, oeuvre.inscriptions, oeuvre.texte',
+        'metier'    :'metier.metier',
+        'materiaux_technique':'materiaux_technique.materiaux_technique',
+        'domaine'   :'domaine.domaine',
+        'artiste'   :'artiste.nom_naissance, artiste.dit',
+        'musee'     :"musee.museo, musee.nom, musee.ville as ville_musee, musee.latitude, musee.longitude"
+    }
+
+JOIN_TABLE = {
+    "metier"                :[("creer","role")],
+    "ville"                 :[("oeuvre","creation_lieux"), ("musee","ville")],
+    "domaine"               :[("concerner","domaine"),],
+    "artiste"               :[("creer","artiste")],
+    "musee"                 :[("oeuvre","lieux_conservation"), ('ville', 'id')],
+    "materiaux_technique"   :[("composer","materiaux")],
+    "oeuvre"                :[("creer","oeuvre"), ("concerner","oeuvre"), ("composer","oeuvre"), ("musee","museo"), ("ville","id")],
+    "composer"              :[("oeuvre","ref"),  ("materiaux_technique","id")],
+    "creer"                 :[("metier","metier_search"), ("oeuvre","ref"),  ("artiste","id")],
+    "concerner"             :[("oeuvre","ref"),  ("domaine","id")],
+}
+
+# ----------------------------------------------------------------------------------
+# %% RECHERCHE EN BDD
+# ----------------------------------------------------------------------------------
+#  %% search_multicriteria
+def search_multicriteria(ville=None, oeuvre=None, musee=None, metier=None, materiaux=None, domaine=None, artiste=None, type_oeuvre=None, search_strategie='AND', search_level=1, limit=None, verbose=0):
     short_name = "search_multicriteria"
-    end_sql = ""
+    sql_end = ""
+    slq_begin = ""
+    sql_from = " FROM "
     nb_cond = 0
 
     join_table = set()
@@ -52,17 +110,31 @@ def search_multicriteria(ville=None, oeuvre=None, musee=None, metier=None, mater
 
     for table_name, value in params.items():
         if value is not None and len(value)>0:
-            (sub_sql, nb_cond2) = _create_cond(table_name=table_name, value=ville, nb_cond=nb_cond, search_strategie=search_strategie, search_level=search_level, verbose=verbose)
+            if nb_cond >0:
+                sql_from += ", "
+            sql_from += table_name
+            
+            (sub_sql, nb_cond2, sub_sql_begin) = _create_cond(table_name=table_name, value=value, nb_cond=nb_cond, search_strategie=search_strategie, search_level=search_level, verbose=verbose)
+
             if len(sub_sql)>0:
-                end_sql += sub_sql
+                sql_end += sub_sql
                 join_table.add(table_name)
                 nb_cond = nb_cond2
                 if verbose>1:
                     print(f'[{short_name}] \tDEBUG : {table_name} : condition {value} -- ADDED')
             elif verbose>0:
                 print(f'[{short_name}] \tWARN : {table_name} : condition {value} -- NOT ADDED')
+            if len(sub_sql_begin)>0:
+                if len(slq_begin)>0:
+                    slq_begin += ","
+                slq_begin += " "+sub_sql_begin
+                if verbose>1:
+                    print(f'[{short_name}] \tDEBUG : {table_name} : cols {sub_sql_begin} -- ADDED')
+            elif verbose>1:
+                print(f'[{short_name}] \tDEBUG : {table_name} : no cols -- ADDED')
+            
         elif verbose>1:
-            print(f'[{short_name}] \DEBUG : {table_name} : no condition value')
+            print(f'[{short_name}] \tDEBUG : {table_name} : no condition value')
     
     if type_oeuvre is not None and len(type_oeuvre)>0:
         sql += f"type LIKE '{type_oeuvre}' "
@@ -70,8 +142,28 @@ def search_multicriteria(ville=None, oeuvre=None, musee=None, metier=None, mater
             sql+= f"{search_strategie} "        
         join_table.add('oeuvre')
         nb_cond += 1
-    
 
+    sql_where, to_add_from = _build_join(join_table=join_table, verbose=verbose)
+    if len(sql_where) == 0:
+        sql_end = " WHERE "+sql_end
+    else:
+        sql_end = " AND "+sql_end
+
+    for t in to_add_from:
+        if nb_cond >0:
+                sql_from += ", "
+        sql_from += t
+
+    sql = "SELECT "+ slq_begin + " " + sql_from + " " +sql_where+ " " + sql_end
+    if limit is not None:
+        sql += f" LIMIT {limit}"
+
+    if verbose>0:
+        print(f'[{short_name}] \tDEBUG : {sql}')
+
+    res = executer_sql(sql=sql, verbose=verbose)
+    return res
+    
 
 #  %% search_on_table
 def search_on_table(table_name, search_str, search_level=1, increase_level=True, limit=0, verbose=0):
@@ -100,29 +192,139 @@ def search_on_table(table_name, search_str, search_level=1, increase_level=True,
     if res is None and increase_level and search_level < 3:
         if verbose>0:
             print(f"[{short_name}]  INFO: no result with leve {search_level}, trying next level.")
-        res = search_on_table(table_name=table_name, search_str=search_str, cols_by_level=cols_by_level, search_level=search_level+1, increase_level=increase_level, limit=limit, verbose=verbose)
+        res = search_on_table(table_name=table_name, search_str=search_str, search_level=search_level+1, increase_level=increase_level, limit=limit, verbose=verbose)
     return res
-
 
 # ----------------------------------------------------------------------------------
 #  %%                      PRIVATE FUNCTIONS
 # ----------------------------------------------------------------------------------
 def _create_cond(table_name, value, nb_cond=0, search_strategie='AND', search_level=1, verbose=0):
     sql = ""
+    slq_begin = ""
     if value is not None and len(value)>0:
         for level, cols in LEVEL_COLS.get(table_name, {}).items():
             if level <= search_level:
                 for col in cols:
                     if nb_cond>0:
                         sql+= f"{search_strategie} "            
+                        slq_begin += ','
                     sql+= f"{col} LIKE '%{value}%' "
                     nb_cond += 1
-    return (sql, nb_cond)
+                    # TODO ce ne sont pas ces colonnes qu'il faut récupérer
+                    slq_begin = SEARCH_RESULT_COLS_BY_TABLE.get(table_name, "")
+
+    return (sql, nb_cond, slq_begin)
+
+def _build_join(join_table, verbose=0):
+    ever_join = set()
+    sql_join = ""
+    to_add_from = set()
+
+    for table_name in join_table:
+        for to_join in join_table:
+            if table_name != to_join and (table_name, to_join) not in ever_join and (to_join, table_name) not in ever_join:
+
+                joinable = _joinable_via(table_name=table_name, table_name2=to_join)
+
+                if joinable is not None and len(joinable)>0:
+                    if joinable == to_join:
+                        sql_join_tp = _join_for_table(table_name=table_name, to_join=to_join, verbose=verbose)
+                        if len(sql_join_tp)>0:
+                            if len(sql_join) == 0:
+                                sql_join += " WHERE "
+                            else:
+                                sql_join += f" AND "
+                            sql_join += f"{sql_join_tp}"
+                            ever_join.add((table_name, to_join))
+                            ever_join.add((to_join, table_name))
+                    # Gestion des relations N:N
+                    else:
+                        sql_join_tp = _join_for_table(table_name=table_name, to_join=joinable, verbose=verbose)
+                        if len(sql_join_tp)>0:
+                            if len(sql_join) == 0:
+                                sql_join += " WHERE "
+                            else:
+                                sql_join += f" AND "
+                            sql_join += f"{sql_join_tp}"
+                            ever_join.add((table_name, joinable))
+                            ever_join.add((joinable, table_name))
+                            to_add_from.add(joinable)
+                            
+                            sql_join_tp = _join_for_table(table_name=to_join, to_join=joinable, verbose=verbose)
+                            if len(sql_join_tp)>0:
+                                if len(sql_join) == 0:
+                                    sql_join += " WHERE "
+                                else:
+                                    sql_join += f" AND "
+                                sql_join += f"{sql_join_tp}"
+                                ever_join.add((to_join, joinable))
+                                ever_join.add((joinable, to_join))
+                                ever_join.add((table_name, to_join)) 
+                                ever_join.add((to_join, table_name))
+    return sql_join,to_add_from 
+
+def _joinable_via(table_name, table_name2, verbose=0):
+    """Regarde si une jointure est possible pour les 2 tables soit en direct, soit via une troisième table pour les liaisons N:N
+
+    Args:
+        table_name (_type_): _description_
+        table_name2 (_type_): _description_
+        verbose (int, optional): _description_. Defaults to 0.
+
+    Returns:
+        _type_: _description_
+    """
+    join1 = JOIN_TABLE.get(table_name)
+    join2 = JOIN_TABLE.get(table_name2)
+    res = ''
+    for tb_name, _ in join1:
+        if tb_name == table_name2:
+            res = table_name2
+        elif tb_name.endswith("er"):
+            for tb_name2, _ in join2:
+                if tb_name2 == tb_name:
+                    res = tb_name2
+        if len(res)>0:
+            break
+    return res
+
+
+
+
+def _join_for_table(table_name, to_join, verbose=0):
+    """Build the equals part between the 2 tables
+
+    Args:
+        table_name (str): the table name
+        to_join (str): the second table name
+
+    Returns:
+        str: the equals str
+    """
+    join_str = ""
+    joins = JOIN_TABLE.get(table_name)
+    for tb_name, col_name in joins:
+        
+        if tb_name == to_join:
+            join_str += f"{tb_name}.{col_name} = "
+    
+            to_join_table_join = JOIN_TABLE.get(tb_name)
+            for tb_name2, col_name2 in to_join_table_join:
+                if tb_name2 == table_name:
+                    join_str += f"{tb_name2}.{col_name2} "
+                    break
+            break
+    return join_str
 
 # ----------------------------------------------------------------------------------
 #  %%                      TEST
 # ----------------------------------------------------------------------------------
 def _test_search_artiste(verbose=1):
+    """Teste la recherche d'artiste avec plusieurs niveaux de recherche
+
+    Args:
+        verbose (int, optional): Log level. Defaults to 1.
+    """
     
     # SELECT * FROM artiste WHERE nom_search LIKE '%HUGUES%' OR nom_naissance LIKE '%HUGUES%' OR dit LIKE '%HUGUES%' LIMIT 100;
     # SELECT * FROM artiste WHERE nom_search LIKE '%RAOUL%' OR nom_naissance LIKE '%RAOUL%' OR dit LIKE '%RAOUL%' LIMIT 100;
@@ -142,6 +344,11 @@ def _test_search_artiste(verbose=1):
         assert nb == expected
 
 def _test_search_oeuvre(verbose=1):
+    """Teste la recherche d'oeuvre avec plusieurs niveaux de recherche
+
+    Args:
+        verbose (int, optional): Log level. Defaults to 1.
+    """
     
     # SELECT * FROM oeuvre WHERE titre LIKE '%HUGUES%' OR inscriptions LIKE '%HUGUES%' OR texte LIKE '%HUGUES%' OR type LIKE '%HUGUES%' OR domaine LIKE '%HUGUES%' LIMIT 100;
     # SELECT * FROM oeuvre WHERE titre LIKE '%RAOUL%' OR inscriptions LIKE '%RAOUL%' OR texte LIKE '%RAOUL%' OR type LIKE '%RAOUL%' OR domaine LIKE '%RAOUL%' LIMIT 100;
@@ -167,11 +374,85 @@ def _test_search_oeuvre(verbose=1):
         nb = len(res)
         assert nb == expected
 
+def _test_search_multicriteria(verbose=1):
+    """Test la recherche multicritères
+
+    Args:
+        verbose (int, optional): Log level. Defaults to 1.
+    """
+    to_test={ 
+        ('ville', 'oeuvre', 'musee', 'metier', 'materiaux', 'domaine', 'artiste', 'type_oeuvre', 'search_strategie', 'search_level'):45,
+        ('BRIEUC', None,   None,   None,       'agate',    None, None, None, 'OR', 1):[],
+        ('BRIEUC', 'RAOUL',   None,   'peintre',  'agate',    None, None, None, 'AND', 1):[],
+        ('BRIEUC', 'GOULVEN',   None,   'peintre',  'agate',    None, None, None, 'OR', 1):[],
+        ('LANNION', None,   None,   None,       None,        None, None, None, 'AND', 1):[],
+        ('BRIEUC', None,   None,   None,       None,        None, None, None, 'AND', 1):[(319, 'Saint-Brieuc', "Côtes-d'Armor", 'Bretagne')],
+        ('BRIEUC', None,   None,   'peintre',  None,        None, None, None, 'AND', 1):[],
+    }
+    limit=100
+    for (ville, oeuvre, musee, metier, materiaux, domaine, artiste, type_oeuvre, search_strategie, search_level), expected in tqdm(to_test.items(), desc="[TEST search_multicriteria]"):
+        # On passe la première ligne qui sert uniquement d'entête pour les valeurs
+        if 'ville' != ville:
+            res = search_multicriteria(ville=ville, oeuvre=oeuvre, musee=musee, metier=metier, 
+                                materiaux=materiaux, domaine=domaine, artiste=artiste, 
+                                type_oeuvre=type_oeuvre, search_strategie=search_strategie, search_level=search_level, limit=limit, verbose=verbose)
+            assert len(res) == len(expected) or len(res) == limit
+            if len(res)>0 and len(res)<limit:
+                assert res == expected
+
+def _test_join_for_table(verbose=1):
+    """Teste la construction automatique des jointures entre les tables
+
+    Args:
+        verbose (int, optional): Log level. Defaults to 1.
+    """
+    to_test = {
+        ("domaine", "concerner") :'concerner.domaine = domaine.id ',
+        ("domaine", "artiste")  :"",
+        ("ville", "creer")      :"",
+        ("musee", "ville")      :'ville.id = musee.ville ',
+        ("ville", "musee")      :'musee.ville = ville.id ',
+        ("oeuvre", "ville")     :'ville.id = oeuvre.creation_lieux ',
+        ("oeuvre", "musee")     :'musee.museo = oeuvre.lieux_conservation ',
+        ("musee", "oeuvre")     :'oeuvre.lieux_conservation = musee.museo ',
+        ("artiste", "creer")    :'creer.artiste = artiste.id ',
+        ("creer", "artiste")    :'artiste.id = creer.artiste ',
+        ("materiaux_technique", "composer")    :'composer.materiaux = materiaux_technique.id ',
+        ("oeuvre", "creer")     :'creer.oeuvre = oeuvre.ref ',
+        ("oeuvre", "concerner") :'concerner.oeuvre = oeuvre.ref ',
+        ("oeuvre", "composer")  :'composer.oeuvre = oeuvre.ref ',
+        ("metier", "creer")     :'creer.role = metier.metier_search ',
+    }
+
+    for (table_name, join_table), expected in tqdm(to_test.items(), desc="[TEST join_for_table]"):
+        res = _join_for_table(table_name=table_name, to_join=join_table, verbose=verbose)
+        assert res == expected
+
+def _test_build_join(verbose=1):
+    """Teste la construction automatique des jointures pour plusieurs tables
+
+    Args:
+        verbose (int, optional): Log level. Defaults to 1.
+    """
+    to_test = {
+        ("oeuvre", "artiste")           :(' WHERE creer.oeuvre = oeuvre.ref  AND creer.artiste = artiste.id ', {'creer'}),
+        ("oeuvre", "artiste", "ville")  :(' WHERE creer.oeuvre = oeuvre.ref  AND creer.artiste = artiste.id  AND ville.id = oeuvre.creation_lieux ', {'creer'}),
+        ("oeuvre", "domaine")           :(' WHERE concerner.oeuvre = oeuvre.ref  AND concerner.domaine = domaine.id ', {'concerner'}),
+        ("oeuvre", "domaine", "artiste"):(' WHERE concerner.oeuvre = oeuvre.ref  AND concerner.domaine = domaine.id  AND creer.oeuvre = oeuvre.ref  AND creer.artiste = artiste.id ', {'concerner', 'creer'}),
+    }
+    for to_join, expected in tqdm(to_test.items(), desc="[TEST build_join]"):
+        res = _build_join(to_join, verbose=0)
+        assert res == expected
+
 # ----------------------------------------------------------------------------------
 # %%                       MAIN
 # ----------------------------------------------------------------------------------
 if __name__ == '__main__':
     verbose = 1
-    _test_search_oeuvre()
-    _test_search_artiste()
+    _test_join_for_table(verbose=verbose)
+    _test_build_join(verbose=verbose)
+    _test_search_multicriteria(verbose=verbose)
+    _test_search_oeuvre(verbose=verbose)
+    _test_search_artiste(verbose=verbose)
+
 
---
+++ dao/request.sql
@@ -0,0 +1,51 @@
+-- Script with all SQL request use for this project (except creation and drop)
+-- 
+-- Project: Cojoden avance
+-- =======
+-- 
+-- __authors__     = ("Aurélie RAOUL")
+-- __contact__     = ("aurelie.raoul@yahoo.fr")
+-- __copyright__   = "MIT"
+-- __date__        = "2022-10-01"
+-- __version__     = "1.0.0"
+
+use cojoden_avance;
+
+-- 191 594 rôles renseignés il s'agit uniquement de PEINTRE ?
+SELECT count(creer.role) as nb, creer.role
+FROM creer
+GROUP BY creer.role
+ORDER BY nb DESC;
+
+-- Il y a 2 672 rôles différents
+SELECT count(distinct(creer.role))
+FROM creer;
+
+-- 191 594 rôles renseignés
+SELECT count(*) as nb
+FROM creer
+WHERE creer.role is not NULL
+ORDER BY nb DESC;
+
+SELECT distinct(creer.oeuvre)
+FROM creer  
+WHERE creer.role  LIKE '%sculpteur%';
+
+SELECT composer.oeuvre
+FROM materiaux_technique, composer
+WHERE composer.materiaux = materiaux_technique.id  
+AND mat_search LIKE '%agate%';
+
+SELECT oeuvre.ref, oeuvre.titre, oeuvre.type, oeuvre.annee_debut, oeuvre.inscriptions, oeuvre.texte FROM oeuvre
+WHERE oeuvre.ref in (
+	SELECT distinct(creer.oeuvre)
+	FROM creer  
+	WHERE creer.role  LIKE '%peintre%')
+  
+AND  oeuvre.ref in (
+  SELECT composer.oeuvre
+  FROM materiaux_technique, composer
+  WHERE composer.materiaux = materiaux_technique.id  
+  AND mat_search LIKE '%agate%')
+  
+LIMIT 100;
